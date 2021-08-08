88801cf04ab65371b2f16f3c05c5dbcf013ef050
eilen-code <160895@unsaac.edu.pe>
2021-08-08 17:08:15 -0500

    primer commit

---
+++ AppPersonaDocker/Controllers/LibrosController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppPersonaDocker.Entity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppPersonaDocker.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public LibrosController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+
+
+        // GET: api/<LibrosController>
+        [HttpGet]
+        public IEnumerable<Libro> Get()
+        {
+            return context.Libro.ToList();
+        }
+
+        // GET api/<LibrosController>/5
+        [HttpGet("{id}")]
+        public Libro Get(int id)
+        {
+            return context.Libro.Find(id);
+        }
+
+        // POST api/<LibrosController>
+        [HttpPost]
+        public void Post(Libro libro)
+        {
+            context.Libro.Add(libro);
+            context.SaveChanges();
+        }
+
+        // PUT api/<LibrosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LibrosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var libro = context.Libro.Find(id);
+            if (libro != null)
+            {
+                context.Libro.Remove(libro);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ AppPersonaDocker/Entity/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppPersonaDocker.Entity
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechLanzamiento { get; set; }
+        public string Autor { get; set; }
+        public int Precio { get; set; }
+    }
+}
